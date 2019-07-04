37ff6cbd5cd3dcc3c5aab22c2cd17a03c1564482
Felippe Miguel <felippe.miguel.silva@gmail.com>
2019-07-03 22:33:21 -0300

    Upload de imagens
    - Adicionado pacote 'multer'
    - Config de uploads criado
    - Rota store do PostController criada
    -

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -5,7 +5,20 @@ module.exports = {
 
     },
 
-    async StorageEvent(req, res) {
-        
-    }
+    async store(req, res) {
+        const { author, place, description, hashtags } = req.body;
+        const { filename: image } = req.file;
+
+        const post = await Post.create({
+            author,
+            place,
+            description,
+            hashtags,
+            image
+        });
+
+        return res.json({
+            post
+        })
+    } 
 }
