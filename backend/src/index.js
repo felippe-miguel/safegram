dfc34796878570aefaa7c761539bbaacfb41240f
Felippe Miguel <felippe.miguel.silva@gmail.com>
2019-07-03 23:32:38 -0300

    Dependência "socket.io" adicionada

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
 mongoose.connect('mongodb+srv://teste:teste@cluster0-vvc6p.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
 });
+
+app.use(cors());
 
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
