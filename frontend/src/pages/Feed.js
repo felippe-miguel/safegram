aae85633f05742c143232e81f9814f21184df2d1
Felippe Miguel <felippe.miguel.silva@gmail.com>
2019-07-04 22:21:22 -0300

    Componente Feed
    - Criando a interface e estilo do componente Feed
    - Correções no Header

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,10 +1,72 @@
 import React, { Component } from 'react';
 
+import './Feed.css';
+
+import more from '../assets/more.svg';
+import like from '../assets/like.svg';
+import comment from '../assets/comment.svg';
+import send from '../assets/send.svg';
+
 class Feed extends Component {
     render() {
         return (
-            <h1>Feed</h1>
-        )
+            <section id="post-list">
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Felippe Miguel</span>
+                            <span className="place">Santo André</span>
+                        </div>
+
+                        <img src={more} alt="Mais" />
+                    </header>
+                    
+                    <img src="http://localhost:3333/files/so_good.jpg" alt=""/>
+
+                    <footer>
+                        <div className="actions">
+                            <img src={like} alt="" />
+                            <img src={comment} alt="" />
+                            <img src={send} alt="" />
+                        </div>
+
+                        <strong>900 curtidas</strong>
+
+                        <p>
+                            AHHHHH,  que delícia cara!!!!!
+                            <span>#delicia #teste #chola</span>
+                        </p>
+                    </footer>
+                </article>
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Felippe Miguel</span>
+                            <span className="place">Santo André</span>
+                        </div>
+
+                        <img src={more} alt="Mais" />
+                    </header>
+                    
+                    <img src="http://localhost:3333/files/so_good.jpg" alt=""/>
+
+                    <footer>
+                        <div className="actions">
+                            <img src={like} alt="" />
+                            <img src={comment} alt="" />
+                            <img src={send} alt="" />
+                        </div>
+
+                        <strong>900 curtidas</strong>
+
+                        <p>
+                            AHHHHH,  que delícia cara!!!!!
+                            <span>#delicia #teste #chola</span>
+                        </p>
+                    </footer>
+                </article>
+            </section>
+        );
     }
 }
 
